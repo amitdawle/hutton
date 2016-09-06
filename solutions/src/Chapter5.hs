1015290843291de581ecf4a9cd10217ac29816a8
Amit Dawle <amit.dawle@gmail.com>
2016-09-06 23:03:27 +0100

    Chapter5, exercise 6

--- solutions/src/Chapter5.hs
+++ solutions/src/Chapter5.hs
@@ -14,3 +14,10 @@ replicate' n a = a : (replicate' (n-1) a)
 -- 5
 pyths :: Int -> [(Int,Int,Int)]
 pyths n = [(x,y,z)| x <-[1..n], y <- [1..n], z <- [x..n], y /= x && (x^2 + y^2 == z^2) ]
+
+--6 
+factors :: Int -> [Int]
+factors n = [x| x <- [1..n] , n `mod` x == 0]
+
+perfects :: Int -> [Int]
+perfects n = [ x | x <- [1..n] , sum (init (factors x)) == x ]
--- solutions/test/Chapter5Spec.hs
+++ solutions/test/Chapter5Spec.hs
@@ -22,7 +22,7 @@ spec = describe "Chapter5" $ do
            it "works for n > 0 and type char" $ do
             replicate' 3 'a' `shouldBe` "aaa"
 
-         describe "Chapter5.pyths'" $ do
+         describe "Chapter5.pyths" $ do
            it "works for n = 0" $ do
             pyths 0  `shouldBe` []
            it "works for n > 0" $ do
@@ -30,3 +30,19 @@ spec = describe "Chapter5" $ do
            it "works for a large n > 0" $ do
             pyths 20 `shouldBe` [(3,4,5),(4,3,5),(5,12,13),(6,8,10),(8,6,10),(8,15,17),(9,12,15),(12,5,13),(12,9,15),(12,16,20),(15,8,17),(16,12,20)]
 
+
+         describe "Chapter5.factors" $ do
+           it "works for 0" $ do
+            factors 0  `shouldBe` []
+           it "works for small numbers" $ do
+            factors 6  `shouldBe` [1,2,3,6]
+           it "works for large numbers" $ do
+            factors 120 `shouldBe` [1,2,3,4,5,6,8,10,12,15,20,24,30,40,60,120]
+
+         describe "Chapter5.perfects" $ do
+           it "works for 0" $ do
+            perfects 0  `shouldBe` []
+           it "works for small numbers" $ do
+            perfects 10  `shouldBe` [6]
+           it "works for large numbers" $ do
+            perfects 500 `shouldBe` [6,28,496]            
