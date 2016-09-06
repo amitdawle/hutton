ba7f85918a4280ce76306c3319e117d3d7570cd1
Amit Dawle <amit.dawle@gmail.com>
2016-09-06 22:43:47 +0100

    Chapter5, ex 5

--- solutions/src/Chapter5.hs
+++ solutions/src/Chapter5.hs
@@ -10,3 +10,7 @@ square n = [(x,y) | x<-[0..n], y<-[0..n],  y /= x]
 replicate' :: Int -> a -> [a]
 replicate' 0 _ = []
 replicate' n a = a : (replicate' (n-1) a)
+
+-- 5
+pyths :: Int -> [(Int,Int,Int)]
+pyths n = [(x,y,z)| x <-[1..n], y <- [1..n], z <- [x..n], y /= x && (x^2 + y^2 == z^2) ]
--- solutions/test/Chapter5Spec.hs
+++ solutions/test/Chapter5Spec.hs
@@ -13,13 +13,20 @@ spec = describe "Chapter5" $ do
             square 0 `shouldBe` []
            it "works n > 0" $ do
             square 2  `shouldBe` [(0,1),(0,2),(1,0),(1,2),(2,0),(2,1)]
-         
 
          describe "Chapter5.replicate'" $ do
            it "works for n = 0" $ do
             replicate' 0  'a' `shouldBe` []
-           it "works n > 0 and type string" $ do
+           it "works for n > 0 and type string" $ do
             replicate' 3 "a" `shouldBe` ["a", "a", "a"]
-           it "works n > 0 and type char" $ do
+           it "works for n > 0 and type char" $ do
             replicate' 3 'a' `shouldBe` "aaa"
+
+         describe "Chapter5.pyths'" $ do
+           it "works for n = 0" $ do
+            pyths 0  `shouldBe` []
+           it "works for n > 0" $ do
+            pyths 10 `shouldBe` [(3,4,5),(4,3,5),(6,8,10),(8,6,10)]
+           it "works for a large n > 0" $ do
+            pyths 20 `shouldBe` [(3,4,5),(4,3,5),(5,12,13),(6,8,10),(8,6,10),(8,15,17),(9,12,15),(12,5,13),(12,9,15),(12,16,20),(15,8,17),(16,12,20)]
 
